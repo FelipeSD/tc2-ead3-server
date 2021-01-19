201bc0fccaaf45aafb793df358328ff99d0842bd
Felipe <felipe98_stz@hotmail..com>
2021-01-19 11:15:01 -0300

    credentials mongoDB removed

--- app/config/db.config.js
+++ app/config/db.config.js
@@ -1,4 +1,4 @@
 module.exports = {
-    url: "mongodb+srv://felipe:felipe@cluster0.imqr6.mongodb.net/test"
+    url: "mongodb+srv://<usuario>:<senha>@cluster0.imqr6.mongodb.net/test"
     // url: "mongodb://localhost:27017/bancoTeste"
 };
